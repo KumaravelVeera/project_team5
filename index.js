038877adb8f5e6a9f8ea728520d6bc8d1741a655
Kumaravel V <145631234+KumaravelVeera@users.noreply.github.com>
2023-11-23 17:29:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var path = require('path');  // Import the 'path' module
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 // Define a route for the home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Home Page HTML.html'));
-
 });
+
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
