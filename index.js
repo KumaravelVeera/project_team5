5f47bbf39903c7f496415b28c7d22b229138d9d1
Kumaravel V <145631234+KumaravelVeera@users.noreply.github.com>
2023-11-24 15:01:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,92 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 const path = require('path');
+const { Sequelize, DataTypes } = require('sequelize');
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+// SQLite setup
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: path.join(__dirname, 'database.sqlite'), // Change the database file path
+});
 
-// Define a route for the home page
+// Define the User model
+const User = sequelize.define('User', {
+  first_name: DataTypes.STRING,
+  last_name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  phone: DataTypes.STRING,
+  password: DataTypes.STRING,
+});
+
+// Sync the model with the database
+sequelize.sync();
+
+app.set('port', process.env.PORT || 5000);
+
+// Routes
+
+// Home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Home Page HTML.html'));
 });
 
-// Define a route for the registration page
+// Registration page
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'signup.html'));
 });
 
-// Handle registration form submission
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const { first_name, last_name, email, phone, password } = req.body;
 
-  // Insert new user into the SQLite database
-  db.run(
-    'INSERT INTO users (first_name, last_name, email, phone, password) VALUES (?, ?, ?, ?, ?)',
-    [first_name, last_name, email, phone, password],
-    (error) => {
-      if (error) {
-        res.redirect('/login');
-      }
-      res.redirect('/login');
-    }
-  );
+  try {
+    await User.create({
+      first_name,
+      last_name,
+      email,
+      phone,
+      password,
+    });
+
+    res.redirect('/login');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Registration failed. Please try again.');
+  }
 });
 
-// Define a route for the registration page
+// Checkout page
+app.post('/checkout', async (req, res) => {
+  // Your checkout logic here using the User model
+});
+
+// Login page
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
-app.post('/login', (req, res) => {
+
+app.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
-  // Check if the user exists in the SQLite database
-  db.get('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (error, user) => {
-    if (error || !user) {
-      res.redirect('/');
+  try {
+    const user = await User.findOne({
+      where: { email, password },
+    });
+
+    if (!user) {
+      return res.status(401).send('Invalid username or password');
     }
 
-    // Redirect to the home page after successful login
     res.redirect('/');
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error during login. Please try again.');
+  }
 });
-
 
 // Define a route for the registration page
 app.get('/checkout', (req, res) => {
@@ -60,7 +95,8 @@ app.get('/checkout', (req, res) => {
 
 // Define a route for the registration page
 app.get('/checkout', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Home Page HTML.html'));
+  console.error(error);
+  res.status(500).send('Error during login. Please try again.');
 });
 
 app.get('/feedback', (req, res) => {
