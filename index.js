8b4a613d83dd81464ab6dfe9f016a821e31b3f41
Kumaravel V <145631234+KumaravelVeera@users.noreply.github.com>
2023-11-24 11:17:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,108 @@
-
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const { BlobServiceClient } = require('@azure/storage-blob');
+const multer = require('multer');
+const path = require('path');
 const bodyParser = require('body-parser');
-const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+
 const app = express();
+const port = 3001;
+
+// Connect to SQLite database
+const db = new sqlite3.Database('users5.db');
+
+// Your Azure Storage account connection string
+const connectionString = 'DefaultEndpointsProtocol=https;AccountName=cloudshell332255291;AccountKey=rIu+Fgw8oN3NRiAII07pMN2JjdfSPXOnv5Uc+jdcvtrHxn80qP41vpiqCea/tD4bquxlEpZoT24V+AStQKoifA==;EndpointSuffix=core.windows.net';
+
+// Create a BlobServiceClient
+const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+
+// Create a container client for a specific container (replace 'containerName' with your container name)
+const containerName = 'firewall';
+const containerClient = blobServiceClient.getContainerClient(containerName);
+
+// Middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Multer middleware for handling file uploads
+const storage = multer.memoryStorage(); // Use memory storage for simplicity
+const upload = multer({ storage: storage });
+
+// ... (existing code)
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'upload.html'));
+});
+// Handle file upload
+app.post('/admin', upload.single('file'), async (req, res) => {
+  const file = req.file.buffer;
+
+  // Get a block blob client with a unique name
+  const blobName = Date.now() + '-' + req.file.originalname;
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+  // Upload the file
+  try {
+    await blockBlobClient.upload(file, file.length);
+    res.send('File uploaded successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('File upload failed');
+  }
+});
 
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true }))
-
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-const express = require('express');
-const { CosmosClient } = require("@azure/cosmos");
-const path = require('path');
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-const endpoint = "YOUR_COSMOS_DB_ENDPOINT";
-const key = "YOUR_COSMOS_DB_KEY";
-const client = new CosmosClient({ endpoint, key });
-const databaseId = "krsuser";
-const containerId = "user1";
-const database = client.database(databaseId);
-const container = database.container(containerId);
+// Create a table for users if it doesn't exist
+db.run(`
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  phone TEXT NOT NULL,
+  password TEXT NOT NULL
+);)`
+);
 
-app.set('port', (process.env.PORT || 5000));
+// Create users table if not exists
+db.run(`
+  CREATE TABLE IF NOT EXISTS cash_on_delivery_details (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    address TEXT,
+    email TEXT,
+    locality_apartment TEXT,
+    pincode TEXT,
+    contact_no TEXT,
+    date TEXT,
+    time_slot TEXT
+  )
+`);
 
-// Define routes
-
-// Home page
+// Define a route for the home page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Home Page HTML.html'));
 });
 
-// Registration page
+// Define a route for the registration page
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'signup.html'));
 });
 
+
+
+// Handle registration form submission
 app.post('/register', async (req, res) => {
   const { first_name, last_name, email, phone, password } = req.body;
 
   try {
-    await container.items.create({
-      first_name,
-      last_name,
-      email,
-      phone,
-      password
-    });
+    // Insert new user into the SQLite database
+    await db.runAsync(
+      'INSERT INTO users (first_name, last_name, email, phone, password) VALUES (?, ?, ?, ?, ?)',
+      [first_name, last_name, email, phone, password]
+    );
 
     res.redirect('/login');
   } catch (error) {
@@ -63,8 +111,42 @@ app.post('/register', async (req, res) => {
   }
 });
 
-// Checkout page
-app.post('/checkout', async (req, res) => {
+
+// Define a route for the registration page
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+// Handle login form submission
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the user exists in the SQLite database
+    const user = await dbGetAsync('SELECT * FROM users WHERE email = ? AND password = ?', [email, password]);
+
+    if (!user) {
+      return res.status(401).send('Invalid username or password');
+    }
+
+    // Redirect to the home page after successful login
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error during login. Please try again.');
+  }
+});
+
+
+
+
+// Define a route for the registration page
+app.get('/checkout', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'checkoutdeetails.html'));
+});
+
+
+app.post('/checkout', (req, res) => {
   const {
     name,
     address,
@@ -76,58 +158,22 @@ app.post('/checkout', async (req, res) => {
     time_slot
   } = req.body;
 
-  try {
-    await container.items.create({
-      name,
-      address,
-      email,
-      locality_apartment,
-      pincode,
-      contact_no,
-      date,
-      time_slot
-    });
-
-    res.redirect('/');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Submission failed. Please try again.');
-  }
-});
-
-// Login page
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const querySpec = {
-      query: "SELECT * FROM c WHERE c.email = @email AND c.password = @password",
-      parameters: [
-        { name: "@email", value: email },
-        { name: "@password", value: password }
-      ]
-    };
-
-    const { resources } = await container.items.query(querySpec).fetchAll();
-
-    if (resources.length === 0) {
-      return res.status(401).send('Invalid username or password');
+  // Insert new details into the SQLite database
+  db.run(
+    'INSERT INTO cash_on_delivery_details (name, address, email, locality_apartment, pincode, contact_no, date, time_slot) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+    [name, address, email, locality_apartment, pincode, contact_no, date, time_slot],
+    (error) => {
+      if (error) {
+        return res.status(500).send('Submission failed. Please try again.');
+      }
+      res.redirect('/');
     }
-
-    res.redirect('/');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error during login. Please try again.');
-  }
+  );
 });
 
-// Other pages
-app.get('/checkout', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'checkoutdeetails.html'));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
 
 app.get('/feedback', (req, res) => {
@@ -142,6 +188,3 @@ app.get('/chat', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'chat.html'));
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`Node app is running at localhost:${app.get('port')}`);
-});
