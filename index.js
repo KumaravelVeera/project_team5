68d397a2f82bad9542ca25f50f489c53c9a655ba
Kumaravel V <145631234+KumaravelVeera@users.noreply.github.com>
2023-11-25 22:11:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -136,10 +136,6 @@ app.get('/aboutus', (req, res) => {
 app.get('/chat', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'chat.html'));
 });
-
-
-
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
